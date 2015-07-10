e4e1c1a4e1dee5a7d536449d8228c44d47f3b884
Thomas Deconinck <137479@supinfo.com>
2015-07-10 21:42:24 +0200

    Update demo + remove backspace behavior

--- demo/autocomplete.js
+++ demo/autocomplete.js
@@ -48,11 +48,12 @@
 
 	var Autocomplete = __webpack_require__(1);
 
-	var test = ["France", "Alaska", "Alabama", "New York"];
+	var country_list = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua &amp; Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia &amp; Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Cape Verde", "Cayman Islands", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cruise Ship", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kuwait", "Kyrgyz Republic", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Mauritania", "Mauritius", "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre &amp; Miquelon", "Samoa", "San Marino", "Satellite", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea", "Spain", "Sri Lanka", "St Kitts &amp; Nevis", "St Lucia", "St Vincent", "St. Lucia", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad &amp; Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks &amp; Caicos", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "Uzbekistan", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"];
+
 	document.addEventListener("DOMContentLoaded", function () {
 	  var search = new Autocomplete({
 	    input: "#search",
-	    data: test
+	    data: country_list
 	    // xhr: {
 	    //   url: 'http://localhost:3000/results',
 	    //   method: 'GET',
@@ -93,6 +94,7 @@
 	var KEY_DOWN = 40;
 	var KEY_UP = 38;
 	var KEY_ENTER = 13;
+	var KEY_DEL = 8;
 
 	var Autocomplete = (function () {
 
@@ -144,16 +146,32 @@
 	        }
 	      });
 
+	      document.addEventListener('keydown', function (e) {
+	        if (e.keyCode === KEY_DEL && _this2.input != document.activeElement) {
+	          e.preventDefault();
+	        }
+	      });
+
 	      // Focus list element
 	      document.addEventListener('keyup', function (e) {
+	        e.preventDefault();
 	        if (_this2.hasResults) {
-	          if (e.keyCode === KEY_DOWN) {
-	            _this2.focusNextResult();
-	          } else if (e.keyCode === KEY_UP) {
-	            _this2.focusPreviousResult();
-	          } else if (e.keyCode === KEY_ENTER) {
-	            _this2.updateValue(document.querySelector('.autocomplete-item--focus').innerText);
-	            _this2.hideResults();
+	          switch (e.keyCode) {
+	            case KEY_DOWN:
+	              _this2.focusNextResult();
+	              break;
+	            case KEY_UP:
+	              _this2.focusPreviousResult();
+	              break;
+	            case KEY_ENTER:
+	              _this2.updateValue(document.querySelector('.autocomplete-item--focus').innerText);
+	              _this2.hideResults();
+	              break;
+	            case KEY_DEL:
+	              // this.input.value = this.input.value.slice(0, this.input.value.length - 1);
+	              _this2.input.focus();
+	              _this2.hideResults();
+	              break;
 	          }
 	        }
 	      });
@@ -246,15 +264,19 @@
 
 	      this.clearResults();
 
-	      this.results.forEach(function (result) {
-	        var div = document.createElement('div');
-	        div.className = 'autocomplete-item';
-	        div.innerHTML = result;
+	      if (this.results.length) {
+	        this.results.forEach(function (result) {
+	          var div = document.createElement('div');
+	          div.className = 'autocomplete-item';
+	          div.innerHTML = result;
 
-	        _this5.resultsContainer.appendChild(div);
-	      });
+	          _this5.resultsContainer.appendChild(div);
+	        });
 
-	      this.showResults();
+	        this.showResults();
+	      } else {
+	        this.hideResults();
+	      }
 	    }
 	  }, {
 	    key: 'updateValue',
